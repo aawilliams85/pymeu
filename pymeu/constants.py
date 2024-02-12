ee09e093c8efda5d71f33b0abc85f8234fbb1b87
Adair Williams <adair.williams85@gmail.com>
2024-02-11 17:48:33 -0800

    Updated device strings

--- pymeu/constants.py
+++ pymeu/constants.py
@@ -1,16 +1,19 @@
 CHUNK_SIZE = 1984
 
 HELPER_VERSIONS = {
-    '11.00.00'
+    '11.00.00',
+    '12.00.00.414.73'
 }
 
 ME_VERSIONS = {
-    '11.00.25.230'
+    '11.00.00.230',
+    '11.00.25.230',
+    '12.00.78.414'
 }
 
 PRODUCT_CODES = {
-    '102',
-    '51'
+    '102',  #PanelView Plus 7
+    '51'    #PanelView Plus 6
 }
 
 PRODUCT_TYPES = {
