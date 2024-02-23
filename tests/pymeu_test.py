687a78b49b486294dae87bc468d8ab068a7178f9
Adair Williams <adair.williams85@gmail.com>
2024-02-22 19:31:22 -0800

    Moved some test configuration to a separate file

--- tests/pymeu_test.py
+++ tests/pymeu_test.py
@@ -1,3 +1,5 @@
+import json
+import os
 import time
 import unittest
 
@@ -6,37 +8,55 @@ from pymeu import MEUtility
 # Turn off sort so that tests run in line order
 unittest.TestLoader.sortTestMethodsUsing = None
 
+
 class pymeu_fast_tests(unittest.TestCase):
     def setUp(self):
-        self.meu = MEUtility('192.168.40.123')
+        self.base_path = os.path.abspath(os.path.dirname(__file__))
+        self.config_path = os.path.join(self.base_path, 'config.json')
+        with open(self.config_path, 'r') as f:
+            self.config_data = json.load(f)
+
+        self.download_folder_path = os.path.join(self.base_path, 'download')
+        self.upload_folder_path = os.path.join(self.base_path, 'upload')
+        self.meu = MEUtility(self.config_data['comms_path']['good'])
         print('')
 
     def test_get_terminal_info(self):
         print(self.meu.get_terminal_info())
 
     def test_download_invalid_ext(self):
+        self.download_file_path = os.path.join(self.download_folder_path, self.config_data['file']['download']['bad_extension'])
         with self.assertRaises(Exception):
-            self.meu.download('C:\\git\\pymeu\\tests\\TestA.xyz')
+            self.meu.download(self.download_file_path)
 
     def test_upload(self):
-        print(self.meu.upload('C:\\git\\pymeu\\tests\\upload\\TestA.mer', overwrite=True))
+        self.upload_file_path = os.path.join(self.upload_folder_path, self.config_data['file']['upload']['good'])
+        print(self.meu.upload(self.upload_file_path, overwrite=True))
 
     def test_upload_all(self):
-        print(self.meu.upload_all('C:\\git\\pymeu\\tests\\upload', overwrite=True))
+        print(self.meu.upload_all(self.upload_folder_path, overwrite=True))
 
     def tearDown(self):
         pass
 
 class pymeu_slow_tests(unittest.TestCase):
     def setUp(self):
-        self.meu = MEUtility('192.168.40.123')
+        self.base_path = os.path.abspath(os.path.dirname(__file__))
+        self.config_path = os.path.join(self.base_path, 'config.json')
+        with open(self.config_path, 'r') as f:
+            self.config_data = json.load(f)
+
+        self.download_folder_path = os.path.join(self.base_path, 'download')
+        self.upload_folder_path = os.path.join(self.base_path, 'upload')
+        self.meu = MEUtility(self.config_data['comms_path']['good'])
         print('')
 
     def test_reboot(self):
         print(self.meu.reboot())
         
     def test_download(self):
-        print(self.meu.download('C:\\git\\pymeu\\tests\\TestA.mer', overwrite=True))
+        self.download_file_path = os.path.join(self.download_folder_path, self.config_data['file']['download']['good'])
+        print(self.meu.download(self.download_file_path, overwrite=True))
         
     def tearDown(self):
         pass
