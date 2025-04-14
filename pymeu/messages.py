47bf3d7d9819f827dab8d0a7696f4a90c5b38104
dmroeder <dmroeder@gmail.com>
2025-04-14 10:16:15 -0700

    Adding initial support for pylogix

--- pymeu/messages.py
+++ pymeu/messages.py
@@ -1,25 +1,29 @@
-import pycomm3
 
-from enum import IntEnum
+from enum import Enum
+from . import comms
 
 # Known CIP class codes that aren't previously defined by pycomm3.
 # Further investigation needed.
-class CipClasses(IntEnum):
+class CipClasses(Enum):
     FUNCTION = int.from_bytes(b'\x04\xFD', byteorder='big')
     REGISTRY = int.from_bytes(b'\x04\xFE', byteorder='big')
     FILE = int.from_bytes(b'\x04\xFF', byteorder='big')
 
 # Known CIP service codes that aren't previously defined by pycomm3.
 # Further investigation needed.
-class CipServices(IntEnum):
+class CipServices(Enum):
+    CREATE = int.from_bytes(b'\x08', byteorder='big')
+    DELETE = int.from_bytes(b'\x09', byteorder='big')
+    GET_ATTRIBUTE_SINGLE = int.from_bytes(b'\x0e', byteorder='big')
+    SET_ATTRIBUTE_SINGLE = int.from_bytes(b'\x10', byteorder='big')
     EXECUTE = int.from_bytes(b'\x50', byteorder='big')
     READ_REGISTRY = int.from_bytes(b'\x51', byteorder='big')
     WRITE_FILE = int.from_bytes(b'\x52', byteorder='big')
     READ_FILE = int.from_bytes(b'\x53', byteorder='big')
 
-def create_transfer_instance(cip: pycomm3.CIPDriver, data):
+def create_transfer_instance(cip: comms.Driver, data):
     return cip.generic_message(
-        service=pycomm3.Services.create,
+        service=CipServices.CREATE.value,
         class_code=CipClasses.FILE,
         instance=0x00,
         request_data=data,
@@ -27,16 +31,16 @@ def create_transfer_instance(cip: pycomm3.CIPDriver, data):
         route_path=None
     )
 
-def delete_transfer_instance(cip: pycomm3.CIPDriver, transfer_instance: int):
+def delete_transfer_instance(cip: comms.Driver, transfer_instance: int):
     return cip.generic_message(
-        service=pycomm3.Services.delete,
+        service=CipServices.DELETE.value,
         class_code=CipClasses.FILE,
         instance=transfer_instance,
         connected=False,
         route_path=None
     )
 
-def read_file_chunk(cip: pycomm3.CIPDriver, transfer_instance: int, data):
+def read_file_chunk(cip: comms.Driver, transfer_instance: int, data):
     return cip.generic_message(
         service=CipServices.READ_FILE,
         class_code=CipClasses.FILE,
@@ -46,7 +50,7 @@ def read_file_chunk(cip: pycomm3.CIPDriver, transfer_instance: int, data):
         route_path=None
     )
 
-def read_registry(cip: pycomm3.CIPDriver, data):
+def read_registry(cip: comms.Driver, data):
     return cip.generic_message(
         service=CipServices.READ_REGISTRY,
         class_code=CipClasses.REGISTRY,
@@ -56,7 +60,7 @@ def read_registry(cip: pycomm3.CIPDriver, data):
         route_path=None
     )
 
-def run_function(cip: pycomm3.CIPDriver, data):
+def run_function(cip: comms.Driver, data):
     return cip.generic_message(
         service=CipServices.EXECUTE,
         class_code=CipClasses.FUNCTION,
@@ -66,7 +70,7 @@ def run_function(cip: pycomm3.CIPDriver, data):
         route_path=None
     )
 
-def write_file_chunk(cip: pycomm3.CIPDriver, transfer_instance: int, data):
+def write_file_chunk(cip: comms.Driver, transfer_instance: int, data):
     return cip.generic_message(
         service=CipServices.WRITE_FILE,
         class_code=CipClasses.FILE,
@@ -76,9 +80,9 @@ def write_file_chunk(cip: pycomm3.CIPDriver, transfer_instance: int, data):
         route_path=None
     )
 
-def get_attr_unk(cip: pycomm3.CIPDriver, data):
+def get_attr_unk(cip: comms.Driver, data):
     return cip.generic_message(
-        service=pycomm3.Services.get_attribute_single,
+        service=CipServices.GET_ATTRIBUTE_SINGLE.value,
         class_code=CipClasses.FILE,
         instance=0x00,
         request_data=data,
@@ -86,9 +90,9 @@ def get_attr_unk(cip: pycomm3.CIPDriver, data):
         route_path=None
     )
 
-def set_attr_unk(cip: pycomm3.CIPDriver, data):
+def set_attr_unk(cip: comms.Driver, data):
     return cip.generic_message(
-        service=pycomm3.Services.set_attribute_single,
+        service=CipServices.SET_ATTRIBUTE_SINGLE.value,
         class_code=CipClasses.FILE,
         instance=0x01,
         request_data=data,
