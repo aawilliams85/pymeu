aef9ba3f269ba9954cbd77a7066f4b1913a6b67d
Colt H <65498634+Colt-H@users.noreply.github.com>
2025-04-17 11:33:41 -0400

    removed extra print statements

--- pymeu/main.py
+++ pymeu/main.py
@@ -52,10 +52,8 @@ class MEUtility(object):
         self.run_at_startup = kwargs.get('run_at_startup', True)
 
         if not os.path.isfile(file_path):
-            print(file_path)
             if ("\\" or "/") not in file_path:
                 file_path = "C:\\Users\\Public\\Documents\\RSView Enterprise\\ME\\Runtime\\" + file_path
-            print(f'new: {file_path}')
 
         with pycomm3.CIPDriver(self.comms_path) as cip:
             file = types.MEFile(self.remote_file_name, self.overwrite, False, file_path)
