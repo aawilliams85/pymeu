5902c6a8185fbc00b9c7bb42a4185ac0f20198a9
aawilliams85 <adair.williams85@gmail.com>
2025-05-18 09:56:17 -0400

    More test refactoring for device,driver pairs.

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from pymeu import MEUtility
@@ -6,6 +7,47 @@ from config import *
 
 # Turn off sort so that tests run in line order
 unittest.TestLoader.sortTestMethodsUsing = None
+
+class download_tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_download_overwrite(self):
+        print('')
+        for device in DEVICES:
+            for driver in DRIVERS:
+                meu = MEUtility(device.comms_path, driver=driver)
+                result = (
+                        f'Device: {device.name}\n'
+                        f'Driver: {driver}\n'
+                        f'Function: download({device.download_paths[0]}, overwrite=True)\n'
+                )
+                print(result)
+                resp = meu.download(device.download_paths[0], overwrite=True)
+                for s in resp.device.log: print(s)
+                print('')
+                self.assertEqual(resp.status, types.MEResponseStatus.SUCCESS)
+                time.sleep(device.boot_time_sec)
+
+    def test_download_as_overwrite(self):
+        print('')
+        for device in DEVICES:
+            for driver in DRIVERS:
+                meu = MEUtility(device.comms_path, driver=driver)
+                result = (
+                        f'Device: {device.name}\n'
+                        f'Driver: {driver}\n'
+                        f'Function: download({device.download_paths[0]}, overwrite=True, remote_file_name={device.mer_files[1]})\n'
+                )
+                print(result)
+                resp = meu.download(device.download_paths[0], overwrite=True, remote_file_name=device.mer_files[1])
+                for s in resp.device.log: print(s)
+                print('')
+                self.assertEqual(resp.status, types.MEResponseStatus.SUCCESS)
+                time.sleep(device.boot_time_sec)
+
+    def tearDown(self):
+        pass
 
 class info_tests(unittest.TestCase):
     def setUp(self):
@@ -65,6 +107,53 @@ class info_tests(unittest.TestCase):
                 print(result)
                 meu.get_terminal_info(print_log=True, silent_mode=True)
                 print('')
+
+    def tearDown(self):
+        pass
+
+class reboot_tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_reboot(self):
+        print('')
+        for device in DEVICES:
+            for driver in DRIVERS:
+                meu = MEUtility(device.comms_path, driver=driver)
+                result = (
+                        f'Device: {device.name}\n'
+                        f'Driver: {driver}\n'
+                        f'Function: reboot()\n'
+                )
+                print(result)
+                resp = meu.reboot()
+                for s in resp.device.log: print(s)
+                print('')
+                self.assertEqual(resp.status, types.MEResponseStatus.SUCCESS)
+                time.sleep(device.boot_time_sec)
+
+    def tearDown(self):
+        pass    
+
+class upload_tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_upload_all(self):
+        print('')
+        for device in DEVICES:
+            for driver in DRIVERS:
+                meu = MEUtility(device.comms_path, driver=driver)
+                result = (
+                        f'Device: {device.name}\n'
+                        f'Driver: {driver}\n'
+                        f'Function: upload_all({UPLOAD_FOLDER_PATH}, overwrite=True)\n'
+                )
+                print(result)
+                resp = meu.upload_all(UPLOAD_FOLDER_PATH, overwrite=True)
+                for s in resp.device.log: print(s)
+                print('')
+                self.assertEqual(resp.status, types.MEResponseStatus.SUCCESS)
 
     def tearDown(self):
         pass
--- tests/pvp5_test.py
+++ tests/pvp5_test.py
@@ -24,18 +24,6 @@ class pvp5_fast_tests(unittest.TestCase):
         self.meu = MEUtility(self.config_data['comms_path']['good'])
         print('')
 
-    def test_get_terminal_info(self):
-        self.meu.get_terminal_info()
-
-    def test_get_terminal_info_print(self):
-        self.meu.get_terminal_info(print_log=True)
-
-    def test_get_terminal_info_print_redacted(self):
-        self.meu.get_terminal_info(print_log=True, redact_log=True)
-
-    def test_get_terminal_info_print_silent(self):
-        self.meu.get_terminal_info(print_log=True, silent_mode=True)
-
     def test_download_invalid_ext(self):
         self.download_file_path = os.path.join(self.download_folder_path, self.config_data['download']['bad_extension'])
         resp = self.meu.download(self.download_file_path)
@@ -44,11 +32,6 @@ class pvp5_fast_tests(unittest.TestCase):
     def test_upload(self):
         self.upload_file_path = os.path.join(self.upload_folder_path, self.config_data['upload']['good'][0])
         resp = self.meu.upload(self.upload_file_path, overwrite=True)
-        for s in resp.device.log: print(s)
-        print(resp.status)
-
-    def test_upload_all(self):
-        resp = self.meu.upload_all(self.upload_folder_path, overwrite=True)
         for s in resp.device.log: print(s)
         print(resp.status)
 
@@ -82,40 +65,6 @@ class pvp5_fast_tests(unittest.TestCase):
 
     def tearDown(self):
         pass
-
-class pvp5_slow_tests(unittest.TestCase):
-    def setUp(self):
-        self.base_path = os.path.abspath(os.path.dirname(__file__))
-        self.config_path = os.path.join(self.base_path, 'config.json')
-        with open(self.config_path, 'r') as f:
-            self.config_data = json.load(f)
-            self.config_data = self.config_data['pvp5']
-
-        self.download_folder_path = os.path.join(self.base_path, 'download')
-        self.upload_folder_path = os.path.join(self.base_path, 'upload')
-        self.meu = MEUtility(self.config_data['comms_path']['good'])
-        print('')
-
-    def test_reboot(self):
-        resp = self.meu.reboot()
-        for s in resp.device.log: print(s)
-        print(resp.status)
-
-    def test_download(self):
-        self.download_file_path = os.path.join(self.download_folder_path, self.config_data['download']['good'][0])
-        resp = self.meu.download(self.download_file_path, overwrite=True)
-        for s in resp.device.log: print(s)
-        print(resp.status)
-
-    def test_download_as(self):
-        self.download_file_path = os.path.join(self.download_folder_path, self.config_data['download']['good'][0])
-        resp = self.meu.download(self.download_file_path, overwrite=True, remote_file_name=self.config_data['download']['good_as'][0])
-        for s in resp.device.log: print(s)
-        print(resp.status)
-
-    def tearDown(self):
-        pass
-        #time.sleep(60)
 
 if __name__ == '__main__':
     unittest.main()
