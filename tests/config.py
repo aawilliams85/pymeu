3e61905decf8c53836abe3b8a66d353aa8ac8c2e
aawilliams85 <adair.williams85@gmail.com>
2025-05-18 09:56:17 -0400

    More test refactoring for device,driver pairs.

--- tests/config.py
+++ tests/config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import os
 
 from pymeu import types
 
@@ -7,8 +8,16 @@ class METestDevice:
     name: str
     comms_path: str
     device_paths: types.MEDevicePaths
+    boot_time_sec: int
+    mer_files: list[str]
+    download_paths: list[str]
 
-# Shared paths
+# Shared paths - computer
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+DOWNLOAD_FOLDER_PATH = os.path.join(BASE_PATH, 'download')
+UPLOAD_FOLDER_PATH = os.path.join(BASE_PATH, 'upload')
+
+# Shared paths - terminal
 HELPER_FILE_NAME = 'RemoteHelper.DLL'
 UPLOAD_LIST_PATH = 'Rockwell Software\\RSViewME\\Runtime\\Results.txt'
 
@@ -19,6 +28,16 @@ PVP5_Device_Paths = types.MEDevicePaths(
     '\\Storage Card',
     f'\\Storage Card\\{UPLOAD_LIST_PATH}'
 )
+PVP5_MER_Files = [
+    'Test_v5_A.mer',
+    'Test_v5_B.mer',
+    'Test_v5_C.mer'
+]
+PVP5_Download_Paths = [
+    os.path.join(DOWNLOAD_FOLDER_PATH, PVP5_MER_Files[0]),
+    os.path.join(DOWNLOAD_FOLDER_PATH, PVP5_MER_Files[1]),
+    os.path.join(DOWNLOAD_FOLDER_PATH, PVP5_MER_Files[2])
+]
 
 # PanelView Plus v6 configuration
 PVP6 = 'PVP6'
@@ -27,10 +46,34 @@ PVP6_Device_Paths = types.MEDevicePaths(
     '\\Application Data',
     f'\\Application Data\\{UPLOAD_LIST_PATH}'
 )
+PVP6_MER_Files = [
+    'Test_v11_A.mer',
+    'Test_v11_B.mer',
+    'Test_v11_C.mer'
+]
+PVP6_Download_Paths = [
+    os.path.join(DOWNLOAD_FOLDER_PATH, PVP6_MER_Files[0]),
+    os.path.join(DOWNLOAD_FOLDER_PATH, PVP6_MER_Files[1]),
+    os.path.join(DOWNLOAD_FOLDER_PATH, PVP6_MER_Files[2])
+]
 
 DEVICES = [
-    METestDevice(PVP5, '192.168.40.124', PVP5_Device_Paths),
-    METestDevice(PVP6, '192.168.40.123', PVP6_Device_Paths)
+    METestDevice(
+        PVP5, 
+        '192.168.40.124', 
+        PVP5_Device_Paths, 
+        75, 
+        PVP5_MER_Files,
+        PVP5_Download_Paths
+    ),
+    METestDevice(
+        PVP6, 
+        '192.168.40.123', 
+        PVP6_Device_Paths, 
+        75, 
+        PVP6_MER_Files,
+        PVP6_Download_Paths
+    )
 ]
 
 DRIVERS = [
