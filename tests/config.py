16f1cfbcfc745230d4f56b8ac1a9033ac2389ade
aawilliams85 <adair.williams85@gmail.com>
2025-05-24 19:14:40 -0400

    Fix upload when using routed path with pylogix.

--- tests/config.py
+++ tests/config.py
@@ -68,7 +68,7 @@ DEVICES = [
     ),
     METestDevice(
         PVP6, 
-        '192.168.40.216,4,192.168.1.20',
+        '192.168.40.104,4,192.168.1.20',
         PVP6_Device_Paths, 
         75, 
         PVP6_MER_Files
