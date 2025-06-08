c9be51704c109b7f3848107309e355dc3660de54
aawilliams85 <adair.williams85@gmail.com>
2025-06-07 22:27:14 -0400

    Add messages for PVP7B flash.

--- pymeu/messages.py
+++ pymeu/messages.py
@@ -2,12 +2,19 @@
 from enum import IntEnum
 from . import comms
 
-class CipClasses(IntEnum):
+class DMKClasses(IntEnum):
+    FIRMWARE_FLASH = int.from_bytes(b'\xA1', byteorder='big')
+
+class DMKServices(IntEnum):
+    FIRMWARE_PREAMBLE = int.from_bytes(b'\x51', byteorder='big')
+    FIRMWARE_CHUNK = int.from_bytes(b'\x52', byteorder='big')
+
+class MEClasses(IntEnum):
     FUNCTION = int.from_bytes(b'\x04\xFD', byteorder='big')
     REGISTRY = int.from_bytes(b'\x04\xFE', byteorder='big')
     FILE = int.from_bytes(b'\x04\xFF', byteorder='big')
 
-class CipServices(IntEnum):
+class MEServices(IntEnum):
     CREATE = int.from_bytes(b'\x08', byteorder='big')
     DELETE = int.from_bytes(b'\x09', byteorder='big')
     GET_ATTRIBUTE_SINGLE = int.from_bytes(b'\x0e', byteorder='big')
@@ -19,63 +26,79 @@ class CipServices(IntEnum):
 
 def create_transfer_instance(cip: comms.Driver, data):
     return cip.generic_message(
-        service=CipServices.CREATE,
-        class_code=CipClasses.FILE,
+        service=MEServices.CREATE,
+        class_code=MEClasses.FILE,
         instance=0x00,
         request_data=data
     )
 
 def delete_transfer_instance(cip: comms.Driver, transfer_instance: int):
     return cip.generic_message(
-        service=CipServices.DELETE,
-        class_code=CipClasses.FILE,
+        service=MEServices.DELETE,
+        class_code=MEClasses.FILE,
         instance=transfer_instance
+    )
+
+def dmk_preamble(cip: comms.Driver, data):
+    return cip.generic_message(
+        service=DMKServices.FIRMWARE_PREAMBLE,
+        class_code=DMKClasses.FIRMWARE_FLASH,
+        instance=0x00,
+        request_data=data
+    )
+
+def dmk_chunk(cip: comms.Driver, data):
+    return cip.generic_message(
+        service=DMKServices.FIRMWARE_CHUNK,
+        class_code=DMKClasses.FIRMWARE_FLASH,
+        instance=0x00,
+        request_data=data
     )
 
 def read_file_chunk(cip: comms.Driver, transfer_instance: int, data):
     return cip.generic_message(
-        service=CipServices.READ_FILE,
-        class_code=CipClasses.FILE,
+        service=MEServices.READ_FILE,
+        class_code=MEClasses.FILE,
         instance=transfer_instance,
         request_data=data
     )
 
 def read_registry(cip: comms.Driver, data):
     return cip.generic_message(
-        service=CipServices.READ_REGISTRY,
-        class_code=CipClasses.REGISTRY,
+        service=MEServices.READ_REGISTRY,
+        class_code=MEClasses.REGISTRY,
         instance=0x00,
         request_data=data
     )
 
 def run_function(cip: comms.Driver, data):
     return cip.generic_message(
-        service=CipServices.EXECUTE,
-        class_code=CipClasses.FUNCTION,
+        service=MEServices.EXECUTE,
+        class_code=MEClasses.FUNCTION,
         instance=0x00,
         request_data=data
     )
 
 def write_file_chunk(cip: comms.Driver, transfer_instance: int, data):
     return cip.generic_message(
-        service=CipServices.WRITE_FILE,
-        class_code=CipClasses.FILE,
+        service=MEServices.WRITE_FILE,
+        class_code=MEClasses.FILE,
         instance=transfer_instance,
         request_data=data
     )
 
 def get_attr_unk(cip: comms.Driver, data):
     return cip.generic_message(
-        service=CipServices.GET_ATTRIBUTE_SINGLE,
-        class_code=CipClasses.FILE,
+        service=MEServices.GET_ATTRIBUTE_SINGLE,
+        class_code=MEClasses.FILE,
         instance=0x00,
         request_data=data
     )
 
 def set_attr_unk(cip: comms.Driver, data):
     return cip.generic_message(
-        service=CipServices.SET_ATTRIBUTE_SINGLE,
-        class_code=CipClasses.FILE,
+        service=MEServices.SET_ATTRIBUTE_SINGLE,
+        class_code=MEClasses.FILE,
         instance=0x01,
         request_data=data
     )
