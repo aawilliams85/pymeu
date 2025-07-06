e6be7fecf77d18e15602df8813fe67249fe3a08a
aawilliams85 <adair.williams85@gmail.com>
2025-07-06 13:07:52 -0400

    Remove kwargs from public functions.

--- examples/cfutility/99_experimental_firmware_flash_pvp7b.py
+++ examples/cfutility/99_experimental_firmware_flash_pvp7b.py
@@ -11,8 +11,8 @@ def progress_callback(description: str, total_bytes: int, current_bytes: int):
     progress = 100* current_bytes / total_bytes
     print(f"{description} progress: {progress:.2f}% complete, {current_bytes} of {total_bytes} bytes.")
 
-meu = CFUtility('YourPanelViewIpAddress')
-meu.flash_firmware_dmk(
+cfu = CFUtility('YourPanelViewIpAddress')
+cfu.flash_firmware(
     firmware_image_path='C:\\YourFolder\\YourFirmwareImage.DMK',
     dry_run=False,
     progress=progress_callback)
