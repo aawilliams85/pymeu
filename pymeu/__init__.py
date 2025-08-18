aec63b43be0f61554ae6fce73522b6fa95f1dad7
aawilliams85 <adair.williams85@gmail.com>
2025-08-17 20:17:17 -0400

    Upped version to 0.4.3

--- pymeu/__init__.py
+++ pymeu/__init__.py
@@ -1,5 +1,5 @@
 from .cfutility import CFUtility
 from .meutility import MEUtility
 
-__version_info__ = (0, 4, 2)
+__version_info__ = (0, 4, 3)
 __version__ = '.'.join(str(x) for x in __version_info__)
